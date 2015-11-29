e41f77d6bd327705734200ea6097a5280b8019fd
Keiichi Lindley <digizn@gmail.com>
2015-11-29 15:32:39 -0700

    Initial Commit with Route Group Definition

---
+++ app/Http/routes.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Foundation\Inspiring;
+
+
+/*
+|--------------------------------------------------------------------------
+| Application Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register all of the routes for an application.
+| It's a breeze. Simply tell Laravel the URIs it should respond to
+| and give it the controller to call when that URI is requested.
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome', ['quote' => Inspiring::quote()]);
+});
+
+
+Route::group(['prefix' => 'api'], function ()
+{
+    Route::any('/', function() {});
+
+    Route::group(['prefix' => 'v1'], function ()
+    {
+        Route::resource('reports',      'ReportController');
+        Route::resource('customers',    'CustomerController');
+        Route::resource('employees',    'EmployeeController');
+        Route::resource('products',     'Productcontroller');
+        Route::resource('transactions', 'TransactionController');
+        Route::resource('reservations', 'ReservationController');
+
+        Route::any('/', function() {});
+    });
+
+});
