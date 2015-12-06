df655deb7634c36b8a72d8fdb5dbd473077280a1
Keiichi Lindley <digizn@gmail.com>
2015-12-06 15:32:49 -0700

    Implement Create Modal for Product Groups and Types

--- resources/views/admin/product-groups.blade.php
+++ resources/views/admin/product-groups.blade.php
@@ -36,13 +36,17 @@
     <div class="row">
         <div class="col-md-12">
             <div class="well well-lg">
+              <span
+                aria-hidden="true"
+                onclick="addItem()"
+                class="glyphicon glyphicon glyphicon-plus pull-right add-item">
+              </span>
               <table class="table table-hover">
                   <thead>
                     <tr>
                       <th>#</th>
                       <th>Name</th>
                       <th>Description</th>
-                      <th>Parent ID</th>
                       <th>Action</th>
                     </tr>
                   </thead>
@@ -52,7 +56,6 @@
                           <th scope="row">{{$i}}</th>
                           <td>{{$productGroups[$i]->name}}</td>
                           <td>{{$productGroups[$i]->description}}</td>
-                          <td>{{$productGroups[$i]->parentID}}</td>
                           <td>
                               <i class="remove-icon"
                                  onclick="removeItem(this)"
@@ -67,6 +70,34 @@
             </div>
         </div>
     </div>
+
+    <div class="modal fade" id="addItemModal" tabindex="-1" role="dialog" aria-labelledby="myModalLabel">
+      <div class="modal-dialog" role="document">
+        <div class="modal-content">
+          <div class="modal-header">
+            <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+            <h4 class="modal-title" id="myModalLabel">Add New Product Group</h4>
+          </div>
+          <form id="addItemForm" data-resource="product-groups">
+            <div class="modal-body">
+              <div class="form-group col-md-12">
+                <label for="name">Name</label>
+                <input type="text" class="form-control" id="name" name="name" placeholder="Name">
+              </div>
+              <div class="form-group col-md-12">
+                <label for="description">Description</label>
+                <textarea class="form-control" id="description"  name="description" rows="3"></textarea>
+              </div>
+            </div>
+            <div class="modal-footer">
+              <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+              <button type="submit" class="btn btn-primary">Create</button>
+            </div>
+          </form>
+        </div>
+      </div>
+    </div>
+
 @endsection
 
 @section('scripts')
