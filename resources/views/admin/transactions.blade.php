2f02df70839eddd2586f58b3040bb26e1cd400c2
Keiichi Lindley <digizn@gmail.com>
2016-05-25 19:13:29 -0600

    Fix bug

--- resources/views/admin/transactions.blade.php
+++ resources/views/admin/transactions.blade.php
@@ -50,12 +50,12 @@
                       <tr id="{{ 'row'.$i }}" class="bg-success">
                           <td scope="row">
                               <a href="sales-history/{{$sales[$i]->id}}">
-                                {{$sales[$i]->transactionCode}}
+                                {{$sales[$i]['transactionCode']}}
                               </a>
                           </td>
-                          <td>{{$sales[$i]->created_at}}</td>
-                          <td>${{$sales[$i]->grand}}</td>
-                          <td>{{$sales[$i]->employee->name . ' '.  $sales[$i]->employee->last_name}}</td>
+                          <td>{{$sales[$i]['created_at']}}</td>
+                          <td>${{$sales[$i]['grand']}}</td>
+                          <td>{{$sales[$i]['employee']['name'] . ' '.  $sales[$i]['employee']['last_name']}}</td>
                       </tr>
                     @endfor
                   </tbody>
